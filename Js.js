d2331b401b8510a8a9b1cdd76eb766ebfe3a4c40
Ruslanhio <ruslanhio2891@gmail.com>
2022-08-15 22:41:26 +0300

    task8

--- Js.js
+++ Js.js
@@ -1,17 +1,8 @@
-function compareEquality(b, c) {
-    if (b === c) { // Change this line
-      return "Equal";
-    }
-    return "No Equal";
-  }
-  
-  compareEquality(36, "36");
-
-  function testNotEqual(val) {
-    if (val!= 99) { // Change this line
+function testStrictNotEqual(val) {
+    if (val!==17) { // Change this line
       return "Not Equal";
     }
     return "Equal";
   }
   
-  testNotEqual(10);
+  testStrictNotEqual(10);
